01b724607ff3ccf4ecdea1226649fab8cc2ba408
ihsanmaulana14 <imaul7853@gmail.com>
2021-09-05 09:22:43 +0700

    update parralax

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,6 +183,12 @@ function parallax(e) {
     const x = (window.innerWidth - e.pageX * speed) / 100;
     const y = (window.innerHeight - e.pageY * speed) / 100;
 
+
+
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+
+    
+    
   });
+  
 }
